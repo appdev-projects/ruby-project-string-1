cc0d9b7d842268efda3126ba65dcb66d11daec9b
Ben Purinton <ben.purinton@gmail.com>
2023-02-17 15:48:29 -0900

    update provided code note for gets

--- gets.rb
+++ gets.rb
@@ -1,5 +1,7 @@
 # Write a program that gets a name (e.g. "alice") from the user, removes
 # any newline character, capitalizes it, and then says "Hello, Alice!"
+# The provided code bellow begins this by storing a user's 
+# provided input in the `name` variable using the `gets` method.
 #
 # Should work similarly to the following:
 # 
